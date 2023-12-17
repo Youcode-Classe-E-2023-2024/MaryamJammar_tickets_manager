4ffdf6dffa616c4f3c55a31e54bff49108208342
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2023-12-17 22:47:57 +0100

    update config and add database model

---
+++ config/config.php
@@ -0,0 +1,12 @@
+<?php
+
+// Informations de connexion à la base de données
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'tickets');
+
+// Autres configurations générales
+define('SITE_URL', 'http://localhost/MaryamJammar_tickets_manager/'); // Remplacez par l'URL de votre site
+
+?>
