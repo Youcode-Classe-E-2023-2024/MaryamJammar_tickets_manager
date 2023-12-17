a18f161f95781c0f5dfc2256c33b0402d6d5409b
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2023-12-17 21:11:32 +0100

    update links

--- app/view/profile.php
+++ app/view/profile.php
@@ -67,6 +67,10 @@
             </a>
           </li>
 
+          <li class="w-full mt-4">
+            <h6 class="pl-6 ml-2 text-xs font-bold leading-tight uppercase opacity-60">Account pages</h6>
+          </li>
+
           <li class="mt-0.5 w-full">
             <a class="py-2.7 text-sm ease-nav-brand my-0 mx-4 flex items-center whitespace-nowrap px-4 transition-colors" href="./User/users.php">
               <div class="shadow-soft-2xl mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-white bg-center stroke-0 text-center xl:p-2.5">
